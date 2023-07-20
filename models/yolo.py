a0ff284cb32c4daf1d00ca368e5d80b3f3e0b570
lerarningblock <116418259+lerarningblock@users.noreply.github.com>
2023-07-20 16:13:13 +0800

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -125,6 +125,8 @@ class Model(nn.Module):
     def forward_once(self, x, profile=False):
         y, dt = [], []  # outputs
         for m in self.model:
+            if isinstance(m, nn.Upsample):
+                m.recompute_scale_factor = None
             if m.f != -1:  # if not from previous layer
                 x = y[m.f] if isinstance(m.f, int) else [x if j == -1 else y[j] for j in m.f]  # from earlier layers
 
